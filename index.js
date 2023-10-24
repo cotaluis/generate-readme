da0b8f7b52644254136f895bd73bf33ac60faebe
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-10-24 12:16:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+const fs = require ('fs');
+const inquirer = require('inquirer');
+const MarkDown = require('./lib/readmeGen');
 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who contributed to your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'What questions do you have about your project?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license do you use for your project?',
+    }
+]
+
+function runQuery() {
+    return inquirer.prompt(questions)
+        .then((answers) => {
+            const mark = MarkDown.generateReadMe(answers)
+            console.log(mark)
+            return answers
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+runQuery()
