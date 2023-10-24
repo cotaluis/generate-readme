857c0bc4f942c92410f2c9b323bec66247170d97
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-10-24 12:12:24 -0700

    Update readmeGen.js

--- readmeGen.js
+++ readmeGen.js
@@ -1,1 +1,37 @@
+class MarkDown {
+    static generateReadMe(answeres) {
+        return `
+        # ${answeres.title}
+
+        ## Table of Contents
+        - [Description](#Description)
+        - [Usage](#Usage)
+        _ [Contributing](#Contributing)
+        - [Installation](#Installation)
+        - [Questions](#Questions)
+        - [License](#License)
+
+        ## Description
+        ${answeres.description}
+
+        ## Usage
+        ${answeres.usage}
+
+        ## Contributing
+        ${answeres.contributing}
+
+        ## Installation
+        ${answeres.installation}
+
+        ## Questions
+        ${answeres.questions}
+
+        ## License
+        ${answeres.license}
+        `
+    }
+}
+
+module.exports = MarkDown;
+
 
